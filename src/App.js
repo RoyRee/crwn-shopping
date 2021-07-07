04072c97adca803310639903c57d4ba87f7b1de9
RoyRee <rahulshrirame6@gmail.com>
2021-07-07 16:24:05 +0530

    created Sign-in component with structure for sign-in and signup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Route } from 'react-router-dom';
+import {Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-
-
+import Header from './components/header/header.component';
+import SignInAndSIgnUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
+      <Header/>
+      <Switch>
       <Route exact  path='/' component={HomePage}/>
       <Route exact path='/shop' component={ShopPage}/>
+      <Route exact path='/signin' component={SignInAndSIgnUpPage}/>
+      </Switch>
     </div>
   );
 }
